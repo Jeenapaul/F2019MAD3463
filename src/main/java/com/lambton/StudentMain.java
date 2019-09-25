c0928ef4c84036c2ff58f3fe13cd75acdddeb3d6
macstudent <jeenapaul1014@gmail.com>
2019-09-25 19:57:16 -0400

    Project completed

---
+++ src/main/java/com/lambton/StudentMain.java
@@ -0,0 +1,18 @@
+package com.lambton;
+
+public class StudentMain {
+    public static void main(String[] args) {
+
+        System.out.println(LambtonStringTools.reverseString("Lambton"));
+        String binaryString = "1101";
+        int decimal = Integer.parseInt(binaryString, 2);
+        System.out.println(decimal);
+
+    }
+
+}
+
+
+
+
+
