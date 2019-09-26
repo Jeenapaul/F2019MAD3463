69ce477f84fdf8d3e15761ba88a169435d783edc
macstudent <jeenapaul1014@gmail.com>
2019-09-25 20:11:05 -0400

    Project creating

---
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -0,0 +1,22 @@
+package com.lambton;
+
+public class LambtonStringTools {
+
+        public static String reverseString(String s) {
+            StringBuilder sb = new StringBuilder(s);
+            sb.reverse();
+            return sb.toString();
+
+            }
+            public static void string()
+            {
+                String s1="Lambton college in toronto";
+
+            }
+        }
+
+
+
+
+
+
